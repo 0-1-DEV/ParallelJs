362dbd1094108870c777587935b62d5676d055af
swanandapps <beingswanand@gmail.com>
2023-12-10 11:18:53 +0530

    sourccode->cleaner->tokenize->AST, ast is in progress

---
+++ Day 2/ast/main.js
@@ -0,0 +1,41 @@
+function createAst(tokens) {
+  //Step 1: init AST
+  let ast = []; //array of nodes
+
+  //Step 2:iterate through tokens
+
+  for (let i = 0; i < tokens.length; i++) {
+    let token = tokens[i];
+
+    //Step 3: Identifying the token
+
+    //there are two type of tokens so far
+    //a. Variables decl, assignement
+    //b. Print keyword
+
+    switch (token) {
+      //figuring out variables in our tokens
+      case "let":
+      case "const":
+      case "var":
+        //handle Variables here
+
+        console.log("Variable found", token);
+
+        break;
+
+      case "print":
+        //handle print statements here
+        console.log("Print Statement found", token);
+
+        break;
+
+      default:
+        //handle unknown tokens
+
+        break;
+    }
+  }
+}
+
+export { createAst };
--- Day 2/interpretor/main.js
+++ Day 2/interpretor/main.js
@@ -1,15 +1,36 @@
 import fs from "fs";
 import chalk from "chalk";
+import { tokenize } from "../lexer/tokenizer.js";
+import { codeCleaner } from "../lexer/cleaners.js";
+import { Parse } from "../parser/main.js";
+
+function InterpretJs(sourcecode) {
+  //Step 1: Read Sourcecode using node fs module
+
+  //Step 2: Cleaning the Sourcecode
+  let result = codeCleaner(sourcecode);
+  console.log("result:", result);
+  //Step 3: Tokenise source code
+
+  //ideal tokens array = [let, x, =,10, const, y , = ,20]
+  let tokens = tokenize(sourcecode);
+  console.log("tokens:", tokens);
+
+  //Step 4: Parser(tokens) -> AST
+
+  let AST = Parse(tokens);
+}
 
 function runFile(filePath) {
-  fs.readFile(filePath, "utf8", (err, data) => {
+  fs.readFile(filePath, "utf8", (err, sourcecode) => {
     if (err) {
       console.error(`Error reading file: ${filePath}`);
       console.error(err);
       return;
     }
 
-    console.log("data:", data);
+    //passing the sourcecode
+    let result = InterpretJs(sourcecode);
   });
 }
 
